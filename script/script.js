79cd7d4b85856189babfb59768e8712bbde21a20
luwat <oluwatobioreniyi@gmail.com>
2022-05-04 07:15:57 +0100

    designing and creating functions for the board

--- script/script.js
+++ script/script.js
@@ -1,7 +1,38 @@
-const container = document.querySelector('.container');
-const grid = document.createElement('div');
-grid.classList.add('.add');
-grid.classList.remove('.remove');
-container.appendChild(grid);
+let color = 'black';
+const populateBoard = (size) => {
+    const board = document.querySelector('.board');
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove())
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-grid.setAttribute('style', 'background: blue; width: 200px; height: 200px');
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
+        let square = document.createElement('div');
+        square.addEventListener('mouseover', colorSquare);
+        square.style.backgroundColor = 'white';
+        board.insertAdjacentElement('beforeend', square);
+    }
+}
+
+populateBoard(16);
+
+const changeSize = (input) => {
+    if (input >= 1 && input <= 100) {
+        populateBoard(input);
+    } else {
+        console.log('Too many or too low');
+    }
+}
+
+function colorSquare() {
+    if (color === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
+}
+
+function changeColor(choice) {
+    color = choice;
+}
